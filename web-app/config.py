336a95755d3c178d2c81ae377e9d12686486688f
Parth <65168574+P-Sach@users.noreply.github.com>
2024-09-12 11:05:09 +0530

    Add files via upload

---
+++ web-app/config.py
@@ -0,0 +1,7 @@
+# config.py
+THRESHOLDS = {
+    'temperature': {'min': 15.0, 'max': 35.0},
+    'humidity': {'min': 30.0, 'max': 70.0},
+    'soil_moisture': {'min': 0.0, 'max': 100.0},
+    'light_intensity': {'min': 100.0, 'max': 2000.0},
+}
